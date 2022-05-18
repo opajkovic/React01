6ed242f3ed7bc45c82f1bb017712611118378bcc
opajkovic <oljaolja170@gmail.com>
2022-05-18 18:21:06 +0200

    added

---
+++ ex03/react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Padawan!</h1>
+      </div>
+    )
+  }
+}
+export default App;
